26c0373d717e6d4e8fa31cd7f0f11d68e91875c2
Seezan Shrestha <seezanshrestha8@gmail.com>
2024-07-08 07:44:47 +0545

    updated logo

--- client/src/components/mini-component/Logo.tsx
+++ client/src/components/mini-component/Logo.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 function Logo() {
   return (
-    <div className='h-200 w-200'>
-        <Image src="/logo.png" alt="CMS Logo" height={200} width={200} className='object-cover'/>
+    <div>
+        <Image src="/logo.png" alt="CMS Logo" height={200} width={200}/>
     </div>
   )
 }
