1484946e3d9db76968bc4f5170415ec808aa428b
Manoj Shrestha <shresthanewar678@gmail.com>
2024-07-30 06:16:47 +0545

    sidebar for editor fixed

---
+++ client/src/app/editor/add-post/page.tsx
@@ -0,0 +1,12 @@
+import SpeechToTextEditor from '@/components/SpeechToTextEditor'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+         <SpeechToTextEditor />
+    </div>
+  )
+}
+
+export default page
---
+++ client/src/app/editor/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import NoOfUsers from '@/components/common-component/NoOfUsers'
+import React from 'react'
+
+function page() {
+  return (
+   <NoOfUsers />
+  )
+}
+
+export default page
