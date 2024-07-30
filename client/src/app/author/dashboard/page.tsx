e5e2006fe78b60526b7cc4a6b1dd430069b37c40
Seezan Shrestha <seezanshrestha8@gmail.com>
2024-07-30 06:30:39 +0545

    jpt

--- client/src/app/author/dashboard/page.tsx
+++ client/src/app/author/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import NoOfUsers from '@/components/common-component/NoOfUsers'
+import SideMenuBarAuthor from '@/components/SideMenuBarAuthor'
 import React from 'react'
 
 function page() {
   return (
     <div>
+        <SideMenuBarAuthor />
         <NoOfUsers />
     </div>
   )
--- client/src/app/author/layout.tsx
+++ client/src/app/author/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { NotificationProvider } from "@/context/NotificationContext";
 import NotificationContainer from "@/components/Notification";
 import '@/app/globals.css'
+import SideMenuBarEditor from "@/components/SideMenuBarEditor";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,13 +19,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <NotificationProvider>
-        <body>
-          <NotificationContainer />
-          {children}
-        </body>
-      </NotificationProvider>
-    </html>
+   <div className="flex">
+    <SideMenuBarEditor/>
+    {children}
+   </div>
   );
 }
