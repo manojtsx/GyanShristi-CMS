c57a188adb7b4e7d3b18c4dd775ea884f15fe2db
Manoj Shrestha <shresthanewar678@gmail.com>
2024-08-21 18:47:35 +0545

    added user and content count

--- client/src/components/common-component/NoOfUsers.tsx
+++ client/src/components/common-component/NoOfUsers.tsx
@@ -1,24 +1,82 @@
-import React from "react";
+"use client";
+import { useNotifications } from "@/context/NotificationContext";
+import React, { useEffect, useState } from "react";
+
+// Call the backend api
+const API = process.env.NEXT_PUBLIC_BACKEND_API;
 
 function NoOfUsers() {
+  const [userCounts, setUserCounts] = useState({
+    admin: 0,
+    editor: 0,
+    author: 0,
+    viewer: 0,
+  });
+
+  const [contentCount, setContentCount] = useState(0);
+  const { addNotification } = useNotifications();
+
+  const getUserCount = async () => {
+    try {
+      const res = await fetch(`${API}api/user/count-user`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!res.ok) {
+        throw new Error("Failed to fetch user counts");
+      }
+
+      const data = await res.json();
+      setUserCounts(data);
+    } catch (err: any) {
+      addNotification(err.message, "error");
+    }
+  };
+
+  const getContentCount = async () => {
+    try {
+      const res = await fetch(`${API}api/content/count-content`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!res.ok) {
+        throw new Error("Failed to fetch content count");
+      }
+
+      const data = await res.json();
+      console.log(data)
+      setContentCount(data.totalContent);
+    } catch (err: any) {
+      addNotification(err.message, "error");
+    }
+  };
+
+  useEffect(() => {
+    getUserCount();
+    getContentCount();
+  }, []);
   
   return (
             <div className="flex gap-4 w-full">
               <div className="rounded-md w-1/4 py-5 text-xl flex flex-col justify-center items-center shadow-all">
                 <p className="text-sm">Number Of Editors</p>
-                <p className="text-sm font-medium">3</p>
+                <p className="text-sm font-medium">{userCounts.editor}</p>
               </div>
               <div className="rounded-md w-1/4 py-5 text-xl flex flex-col justify-center items-center shadow-all">
                 <p className="text-sm">Number Of Authors</p>
-                <p className="text-sm font-medium">3</p>
+                <p className="text-sm font-medium">{userCounts.author}</p>
               </div>
               <div className="rounded-md w-1/4 py-5 text-xl flex flex-col justify-center items-center shadow-all">
                 <p className="text-sm">Number Of Contents</p>
-                <p className="text-sm font-medium">3</p>
+                <p className="text-sm font-medium">{contentCount}</p>
               </div>
               <div className="rounded-md w-1/4 py-5 text-xl flex flex-col justify-center items-center shadow-all">
                 <p className="text-sm">Number Of Viewers</p>
-                <p className="text-sm font-medium">3</p>
+                <p className="text-sm font-medium">{userCounts.viewer}</p>
               </div>
             </div>  
               
