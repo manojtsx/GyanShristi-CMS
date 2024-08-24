cd741774a952c476f25dcd304db5916f8a64280c
Manoj Shrestha <shresthanewar678@gmail.com>
2024-08-24 13:46:18 +0545

    added loading

--- client/src/components/admin-component/SideMenuBarAdmin.tsx
+++ client/src/components/admin-component/SideMenuBarAdmin.tsx
@@ -12,9 +12,11 @@ import { BsChevronDown } from "react-icons/bs";
 import { HiOutlineLogout } from "react-icons/hi";
 import Logout from "../Logout";
 import { useRouter } from "next/navigation";
+import { useAuth } from "@/context/AuthContext";
 
 const SideMenuBarAdmin = () => {
   const router = useRouter();
+  const {user} = useAuth();
   const [open, setOpen] = useState(true);
   const [userMenuOpen, setUserMenuOpen] = useState(false); //submenu
   const [logoutModalOpen, setLogoutModalOpen] = useState(false); // State for modal
@@ -138,7 +140,7 @@ const SideMenuBarAdmin = () => {
           alt="Logo"
         />
         <p className={` text-[#D9D9D9] mt-2 ${!open && "hidden"} duration-300`}>
-          Usha Gurung
+          {user?.name || "User"}
         </p>
       </div>
       <div
