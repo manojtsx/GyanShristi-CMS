b1af0fbe224024de385e50e6ae5b67bd0b0fc846
Manoj Shrestha <shresthanewar678@gmail.com>
2024-08-24 14:44:34 +0545

    love you

---
+++ client/src/components/viewer-component/ViewerLatestContent.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import React from 'react'
+
+
+function ViewerLatestContent() {
+  return (
+    <div>
+        <div>
+            <div>
+            <div>
+            <p>Why is Photosynthesis Important?</p>
+            <p>Get Updated with GyanShristi</p>
+            </div>
+            <Image src='/photosynthesis.jpeg' alt='Profile Picture' width='500' height='500' className="h-24 w-12"/>
+            </div>
+            <button>Read More</button>
+            <div>
+                <div>
+                <Image src='/GirlProfile.jpg' alt='Profile Picture' width='500' height='500' className="h-10 w-10 rounded-full"/>
+                <p>Seezan Shrestha</p>
+                </div>
+                <div>
+                    <p>Science</p>
+                    <p>jan 24, 2024</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ViewerLatestContent
