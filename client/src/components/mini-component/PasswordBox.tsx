29e1e957ac8832f8f08cdd799913ad2265ac5845
Manoj Shrestha <shresthanewar678@gmail.com>
2024-09-21 20:01:10 +0545

    View and Show Button in Password Box

--- client/src/components/mini-component/PasswordBox.tsx
+++ client/src/components/mini-component/PasswordBox.tsx
@@ -1,16 +1,40 @@
-import React from 'react'
-type PasswordBoxProps={
-    name: string;
-    value: string;
-    placeholder: string;
-    onChange : (e : React.ChangeEvent<HTMLInputElement>) => void;
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
+type PasswordBoxProps = {
+  name: string;
+  value: string;
+  placeholder: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
-function PasswordBox({name,value,placeholder, onChange} : PasswordBoxProps) {
+
+function PasswordBox({ name, value, placeholder, onChange }: PasswordBoxProps) {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
   return (
-    <>
-    <input type="password" name={name} value={value} placeholder={placeholder} onChange={onChange} className="bg-white border-none rounded-md shadow-md w-[300px] dark:bg-[#2C2C2C] dark:border-[#303030] dark:text-white"/>
-  </>
-  )
+    <div className="relative w-[300px]">
+      <input
+        type={isPasswordVisible ? 'text' : 'password'}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+        className="bg-white border-none rounded-md shadow-md w-full dark:bg-[#2C2C2C] dark:border-[#303030] dark:text-white"
+      />
+      <button
+        type="button"
+        onClick={togglePasswordVisibility}
+        className="absolute right-2 top-2 text-gray-500 dark:text-gray-300"
+      >
+        <FontAwesomeIcon icon={isPasswordVisible ? faEyeSlash : faEye} />
+      </button>
+    </div>
+  );
 }
 
-export default PasswordBox
+export default PasswordBox;
