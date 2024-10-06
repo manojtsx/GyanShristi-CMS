cc8751c5011adf95e194b086153eeb82644cddb1
Manoj Shrestha <shresthanewar678@gmail.com>
2024-10-06 06:39:29 +0545

    changed server

--- server/configs/corsConfig.js
+++ server/configs/corsConfig.js
@@ -1,9 +1,10 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const FRONTEND_URL= process.env.FRONTEND_URL;
+const HOSTED_URL= process.env.FRONTEND_HOSTED_URL;
 
 const corsOption = {
-    origin: FRONTEND_URL,
+    origin: [FRONTEND_URL, HOSTED_URL],
     optionsSuccessStatus: 200,
     methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
     allowHeaders: "Content-Type, Authorization",
