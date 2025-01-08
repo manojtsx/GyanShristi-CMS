8000b9ac7dec6b12b8b8638ebba5afef4417cd04
Manoj Shrestha <shresthanewar678@gmail.com>
2025-01-08 17:13:28 +0545

    solved not showing change to editor in tables

--- client/src/components/admin-component/ViewerTable.tsx
+++ client/src/components/admin-component/ViewerTable.tsx
@@ -233,7 +233,7 @@ function ViewerTable() {
                   </button>
                 }
                 {
-                  user.role === "admin" || user.role === "editor" &&
+                 ( user.role === "admin" || user.role === "editor") &&
                   <button
                     onClick={() => handleRoleChange('editor')}
                     className="w-full bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded mb-2 transition duration-300"
